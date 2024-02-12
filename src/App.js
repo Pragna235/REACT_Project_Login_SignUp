f6f7ea16fa8ec4bbb58f37a86809da797c1d0ec9
Pragna235 <yanduripragna2003@gmail.com>
2024-02-12 19:11:58 +0530

    React Login Sign Up Page

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import { LoginSignup } from './Components/LoginSignup/LoginSignup';
+
+function App() {
+  return (
+    <div >
+      <LoginSignup/>
+    </div>
+  );
+}
+
+export default App;
